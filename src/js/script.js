874628635fcc2223e774d8741828e0c7a195dd20
volodymyr kuznietsov <kuz.v@ukr.net>
2024-04-29 18:55:49 +0300

    moderate styles

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,8 @@
 const menuButton = document.querySelector('.header__menu-btn');
 const sideBarMobile = document.querySelector('.header__sidebar-mobile');
 
-console.log(menuButton);
-console.log(sideBarMobile);
+// console.log(menuButton);
+// console.log(sideBarMobile);
 
 menuButton.addEventListener('click', () => {
    sideBarMobile.classList.toggle('open');
