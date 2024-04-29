8c2ea4206a891542eaf9d1a393095ec32f13d7d2
volodymyr kuznietsov <kuz.v@ukr.net>
2024-04-29 21:21:47 +0300

    upgrade html

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,33 @@
 const investInput = document.querySelector('.deposit__range__invest-input');
 const investOutput = document.querySelector('.deposit__range__invest-output');
+const incomeInput = document.querySelector('.deposit__range__income-input');
+const incomeOutput = document.querySelector('.deposit__range__income-output');
 
 const sideBarMobile = document.querySelector('.header__sidebar-mobile');
 
-investInput.addEventListener('change', () => {
-   investOutput.textContent = investInput.value;
+const multiplyier = 7.5;
+
+investInput.addEventListener('input', () => {
+   const valueIn = investInput.value;
+   investOutput.textContent = valueIn;
+
+   const profitSumm = Math.round(valueIn * multiplyier);
+   //    incomeInput.setAttribute('value', profitSumm);
+   incomeInput.value = profitSumm;
+   incomeOutput.textContent = profitSumm;
 });
+
+function increaseValue(less, more) {
+   while (less < more) {
+      less += 1;
+   }
+
+   return;
+}
+
+// incomeInput.addEventListener('input', () => {
+//    incomeOutput.textContent = incomeInput.value;
+// });
 
 // const showingTopBarOnScroll = () => {
 //    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
