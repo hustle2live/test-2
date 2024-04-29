156ec73337d4e705ed945f4e08131710635cf09d
volodymyr kuznietsov <kuz.v@ukr.net>
2024-04-29 22:08:00 +0300

    Updates

---
+++ js/buttons.js
@@ -0,0 +1,1 @@
+const menuButton=document.querySelector(".header__menu-btn"),sideBarMobile=document.querySelector(".header__sidebar-mobile");menuButton.addEventListener("click",()=>{sideBarMobile.classList.toggle("open")});
