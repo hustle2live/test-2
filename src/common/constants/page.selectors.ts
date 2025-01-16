8fe727c588230214e1867237436f60bae1f7a6d9
hustle2live <kuz.v@ukr.net>
2025-01-16 16:56:46 +0200

    feat: added page constants

--- src/common/constants/page.selectors.ts
+++ src/common/constants/page.selectors.ts
@@ -2,6 +2,14 @@ const ActionSelectors = {
    BUTTON_LIKE: 'button.controls__btn > .icon-like',
 } as const;
 
+const DataSelectors = {
+   USER_AGE: '.users__item .age> ',
+   AFTER_LOGIN_POPUP: '.first-sympathy-popup',
+   AFTER_LOGIN_BUTTON_NEXT: '.first-sympathy-popup button.continue',
+   AFTER_LOGIN_BUTTON_CLOSE:
+      '.first-sympathy-popup .first-sympathy-popup__close',
+} as const;
+
 const FormSelectors = {
    LOGIN: {
       INPUT_LOGIN: '.signin__item input[type="text"]',
@@ -14,4 +22,4 @@ const URLPathSelectors = {
    LOGIN: '/signin',
 } as const;
 
-export { ActionSelectors, FormSelectors, URLPathSelectors };
+export { ActionSelectors, FormSelectors, URLPathSelectors, DataSelectors };
